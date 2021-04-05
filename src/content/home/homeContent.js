94aaddce6d2539eba4ecc4f82c3891a35612f164
Eduard Jacobs <edoter92@gmail.com>
2021-04-04 23:43:00 -0700

    home page content

--- src/content/home/homeContent.js
+++ src/content/home/homeContent.js
@@ -1,3 +1,15 @@
-export const heroContent = () => {
-  return {}
+export const heroContent = hero => {
+  return {
+    title: "PROFESSIONAL SKILLS",
+    header: "Full Stack Software Engineer",
+    subtitle: "Experianced in building full stack applications",
+    image: hero.childImageSharp.fluid,
+    alt: "page-wallets-alt",
+    buttons: [
+      {
+        path: "/wallets/find-wallet/",
+        content: "Find Out More",
+      },
+    ],
+  }
 }
