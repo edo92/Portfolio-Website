4a22df8ba8cbca39357e7b17ab056ff992bcfd0e
Eduard Jacobs <edoter92@gmail.com>
2021-04-04 23:42:34 -0700

    hero page component

---
+++ src/content/home/homeContent.js
@@ -0,0 +1,3 @@
+export const heroContent = () => {
+  return {}
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,34 @@
 import React from "react"
+// import styled from "styled-components"
+import { graphql } from "gatsby"
 
 import { Page } from "../components/SharedStyles"
 import PageMetadata from "../components/PageMetadata"
+import PageHero from "../components/PageHero"
+import content from "../content/home/homeContent"
 
-const HomePage = () => {
+const HomePage = ({ data }) => {
   return (
     <Page>
       <PageMetadata
         title={"page-index-meta-title"}
         description={"page-index-meta-description"}
       />
+      <PageHero content={content.heroContent(data.hero)} />
     </Page>
   )
 }
 
 export default HomePage
+
+export const query = graphql`
+  query {
+    hero: file(relativePath: { eq: "home/home-hero.png" }) {
+      childImageSharp {
+        fluid(maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
