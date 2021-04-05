72e7779d33eadeb9ab49bb92ac671ccdd9d37e4f
Eduard Jacobs <edoter92@gmail.com>
2021-04-05 14:53:22 -0700

    init

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "./src/components/Layout"
-
-import Prism from "prism-react-renderer/prism"
-;(typeof global !== "undefined" ? global : window).Prism = Prism
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
