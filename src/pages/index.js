7194d9fc69ea8d882a2eb5eef68d8983665c0f95
Eduard Jacobs <edoter92@gmail.com>
2021-04-04 23:04:45 -0700

    page medatadata component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react"
+// import styled from "styled-components"
+// import { graphql } from "gatsby"
+
 import { Page } from "../components/SharedStyles"
+import PageMetadata from "../components/PageMetadata"
 
 const HomePage = () => {
   return (
     <Page>
+      <PageMetadata
+        title={"page-index-meta-title"}
+        description={"page-index-meta-description"}
+      />
       <h1>homePage</h1>
     </Page>
   )
