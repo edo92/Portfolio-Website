1cd9a9ee93852d1751d526c3c62065fd080fbc5b
Eduard Jacobs <edoter92@gmail.com>
2021-04-04 22:07:01 -0700

    theme setup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,10 @@
 import React from "react"
 
+// import PropTypes from "prop-types"
+// import "../styles/layout.css"
+
+// import styled, { ThemeProvider } from "styled-components"
+
 const Layout = props => {
   return <div>{props.children}</div>
 }
