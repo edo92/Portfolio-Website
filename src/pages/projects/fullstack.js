3a83a1c51ce007ac32aa5cd67b20d85f207fe3d5
Eduard Jacobs <edoter92@gmail.com>
2021-05-23 21:49:20 -0700

    projects list

--- src/pages/projects/fullstack.js
+++ src/pages/projects/fullstack.js
@@ -1,11 +1,93 @@
 import React from "react"
+import styled from "styled-components"
+import { graphql } from "gatsby"
 
-const ProjectsFullStack = () => {
+import PageMetadata from "../../components/PageMetadata"
+import * as content from "../../content/projects/content"
+import ActionCard from "../../components/ActionCard"
+import { fullStack } from "../../content/projects/projectlist"
+
+import {
+  CardContainer,
+  Content,
+  Page,
+  H2,
+  GrayContainer,
+} from "../../components/SharedStyles"
+
+const StyledCardContainer = styled(CardContainer)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  margin: 0rem;
+  @media (max-width: ${props => props.theme.breakpoints.l}) {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-template-columns: 1fr;
+  }
+`
+
+const SectionIntro = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 3rem 0 1rem 0;
+`
+
+const StyledCard = styled(ActionCard)`
+  min-width: 480px;
+  margin: 1rem;
+  border-radius: 2px;
+  border: 1px solid ${props => props.theme.colors.text};
+  background: ${props => props.theme.colors.background};
+  box-shadow: ${props => props.theme.colors.cardBoxShadow};
+  @media (max-width: ${props => props.theme.breakpoints.l}) {
+    margin: 0;
+    min-width: min(100%, 240px);
+  }
+`
+
+const ProjectsFullStack = ({ data }) => {
   return (
-    <div>
-      <h1>Projects Full stack</h1>
-    </div>
+    <Page>
+      <PageMetadata
+        title={content.metadata.title}
+        description={content.metadata.description}
+      />
+      <GrayContainer>
+        <Content id="snippets-projects">
+          <SectionIntro>
+            <H2>Full Stack Applications</H2>
+          </SectionIntro>
+          <StyledCardContainer>
+            {fullStack.projects(data).map((card, idx) => (
+              <StyledCard
+                key={idx}
+                title={card.title}
+                description={card.description}
+                to={card.to}
+                image={card.image}
+                alt={card.alt}
+              />
+            ))}
+          </StyledCardContainer>
+        </Content>
+      </GrayContainer>
+    </Page>
   )
 }
 
 export default ProjectsFullStack
+
+export const query = graphql`
+  query {
+    wifi: file(relativePath: { eq: "projects/wifi.png" }) {
+      childImageSharp {
+        fixed(width: 900) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
--- src/pages/projects/snippets.js
+++ src/pages/projects/snippets.js
@@ -3,15 +3,15 @@ import styled from "styled-components"
 import { graphql } from "gatsby"
 
 import PageMetadata from "../../components/PageMetadata"
-import ActionCard from "../../components/ActionCard"
 import * as content from "../../content/projects/content"
-import { projectslist } from "../../content/projects/projectlist"
+import ActionCard from "../../components/ActionCard"
+import { snippets } from "../../content/projects/projectlist"
+
 import {
   CardContainer,
   Content,
   Page,
   H2,
-  Divider,
   GrayContainer,
 } from "../../components/SharedStyles"
 
@@ -29,6 +29,12 @@ const StyledCardContainer = styled(CardContainer)`
   }
 `
 
+const SectionIntro = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 3rem 0 1rem 0;
+`
+
 const StyledCard = styled(ActionCard)`
   min-width: 480px;
   margin: 1rem;
@@ -42,42 +48,19 @@ const StyledCard = styled(ActionCard)`
   }
 `
 
-const SectionIntro = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 3rem 0 1rem 0;
-`
-
-const SnippetsPage = ({ data }) => {
-  return (
-    <Page>
-      <PageMetadata
-        title={content.metadata.title}
-        description={content.metadata.description}
-      />
-      <GrayContainer>
-        <Content id="snippets-projects">
-          <SectionIntro>
-            <H2>Snippets</H2>
-          </SectionIntro>
-          <StyledCardContainer>
-            {/* {projectslist.snippets(data).map((card, idx) => (
-              <StyledCard
-                key={idx}
-                title={card.title}
-                description={card.description}
-                to={card.to}
-                image={card.image}
-                alt={card.alt}
-              />
-            ))} */}
-          </StyledCardContainer>
-        </Content>
-      </GrayContainer>
-      <Divider />
+const ProjectsSnippets = ({ data }) => (
+  <Page>
+    <PageMetadata
+      title={content.metadata.title}
+      description={content.metadata.description}
+    />
+    <GrayContainer>
       <Content id="snippets-projects">
+        <SectionIntro>
+          <H2>Server Side Applications</H2>
+        </SectionIntro>
         <StyledCardContainer>
-          {projectslist.projects(data).map((card, idx) => (
+          {snippets.projects(data).map((card, idx) => (
             <StyledCard
               key={idx}
               title={card.title}
@@ -89,36 +72,15 @@ const SnippetsPage = ({ data }) => {
           ))}
         </StyledCardContainer>
       </Content>
-    </Page>
-  )
-}
+    </GrayContainer>
+  </Page>
+)
 
-export default SnippetsPage
+export default ProjectsSnippets
 
 export const query = graphql`
   query {
-    nodearchitecture: file(relativePath: { eq: "backend/shuffled.png" }) {
-      childImageSharp {
-        fixed(width: 900) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    awsarchitecture: file(relativePath: { eq: "home/projicon.png" }) {
-      childImageSharp {
-        fixed(width: 900) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
     wifi: file(relativePath: { eq: "projects/wifi.png" }) {
-      childImageSharp {
-        fixed(width: 900) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    azureCognitive: file(relativePath: { eq: "projects/azure-cognitive.png" }) {
       childImageSharp {
         fixed(width: 900) {
           ...GatsbyImageSharpFixed
